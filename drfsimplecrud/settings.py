9773f232ab3af56429c68f0b3bb9ab8ff701278e
Harol Mauricio Gómez Zapata <hgomezz@misena.edu.co>
2024-04-26 17:08:37 -0500

    corregida línea 30: DEBUG = 'RENDER'

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='mi clave secreta')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG 'RENDER' not in os.environ
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
